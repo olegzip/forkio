a2e4f6c62cb7c78717ea1c920ac764f4fe0cf045
olegzip <olegzip@ukr.net>
2018-10-29 01:08:34 +0200

    Section Features done

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,56 @@
-const gulp = require('gulp'),
+let gulp = require('gulp'),
     browserSync = require('browser-sync').create(),
     sass = require('gulp-sass'),
     autoprefixer = require('gulp-autoprefixer'),
     concat = require('gulp-concat'),
     cleanCSS = require('gulp-clean-css'),
+    pump = require('pump'),
     uglify = require('gulp-uglify'),
-    pump = require('pump');
+    clean = require('gulp-clean');
+imagemin = require('gulp-imagemin');
 
 gulp.task('copy-html', () => {
     return gulp.src('./src/**/*.html')
-        .pipe(gulp.dest('./dist/'))
+        .pipe(gulp.dest('./dist/'));
 });
 
-gulp.task('sass', () => {
+gulp.task('imagemin', () =>
+    gulp.src('src/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'))
+);
+
+gulp.task('clean-dist', () => {
+    return gulp.src('./dist/', {read: false})
+        .pipe(clean());
+});
+
+// tasks for CSS files
+
+gulp.task('clean-css', () => {
+    return gulp.src('./src/css/', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('sass', ['clean-css'], () => {
     return gulp.src('./src/scss/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./src/css/'))
+        .on('error', function (err) {
+            console.log(err.toString());
+            this.emit('end');
+        })
+        .pipe(gulp.dest('./src/css/'));
 });
-
 gulp.task('autoprefix', ['sass'], () => {
     return gulp.src('./src/css/**/*.css')
         .pipe(autoprefixer({
             browsers: ['last 2 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('./src/css/'));
-})
+        .pipe(gulp.dest('./src/css/'))
+});
 
-gulp.task('concat-css', ['autoprefix'], function () {
+gulp.task('concat-css', ['autoprefix'], () => {
     return gulp.src('./src/css/**/*.css')
         .pipe(concat('style.css'))
         .pipe(gulp.dest('./src/css/'));
@@ -39,12 +62,19 @@ gulp.task('minify-css', ['concat-css'], () => {
         .pipe(gulp.dest('./src/css/'));
 });
 
-gulp.task('copy-css', ['concat-css'], () =>{
-   return gulp.src('./src/css/**/*.css')
-       .pipe(gulp.dest('./dist/css/'))
+gulp.task('copy-css', ['minify-css'], () => {
+    return gulp.src('./src/css/style.css')
+        .pipe(gulp.dest('./dist/css/'))
 });
 
-gulp.task('concat-js', function () {
+// tasks for JS files
+
+gulp.task('clean-js', () => {
+    return gulp.src('./dist/js/script.js', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('concat-js', ['clean-js'], () => {
     return gulp.src('./src/js/**/*.js')
         .pipe(concat('script.js'))
         .pipe(gulp.dest('./src/js/'));
@@ -60,19 +90,29 @@ gulp.task('minify-js', ['concat-js'], (cb) => {
     );
 });
 
-gulp.task('copy-js', ['minify-js'], () =>{
+gulp.task('copy-js', ['minify-js'], () => {
     return gulp.src('./src/js/script.js')
         .pipe(gulp.dest('./dist/js/'))
 });
 
-gulp.task('serve', ['copy-html', 'copy-css', 'copy-js'], () => {
+gulp.task('serve', ['copy-html', 'copy-css', 'copy-js', 'imagemin'], () => {
     browserSync.init({
         server: {
             baseDir: "./dist"
         }
     });
+
     gulp.watch('./src/**/*.html', ['copy-html']).on('change', browserSync.reload);
     gulp.watch('./src/scss/**/*.scss', ['copy-css']).on('change', browserSync.reload);
     gulp.watch('./src/js/**/*.js', ['copy-js']).on('change', browserSync.reload);
+});
 
+gulp.task('dev', ['clean-dist'], () => {
+    gulp.start('serve');
+});
+
+gulp.task('build', ['clean-dist'], () => {
+    gulp.start('copy-html');
+    gulp.start('copy-css');
+    gulp.start('copy-js');
 });
